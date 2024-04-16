4170d1b37cf50b346dd00590a7f97e4f892b6c4f
UnshiftedSet54 <smlrossales@gmail.com>
2024-04-16 19:26:58 +0100

    Added server logic

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public', 'calcIntro.html'))
+})
+
+app.get('/calculator', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public', 'calculator.html'))
+})
+
+app.get('/*', (req, res) => {
+  res.status(404).json({msg: "Not Found"})
+})
+
+app.listen(PORT, () => console.log(`Listening at port ${PORT}`))
+
+
